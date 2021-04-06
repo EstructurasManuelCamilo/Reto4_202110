01952d953cd59f81615b93afc33c76e5fcb2b6a9
Manuel Gallegos <m.gallegos@uniandes.edu.co>
2021-04-06 15:21:11 -0500

    carpeta

--- src/model/data_structures/TablaHashLinearProbing.java
+++ src/model/data_structures/TablaHashLinearProbing.java
@@ -152,24 +152,46 @@ public class TablaHashLinearProbing <K extends Comparable<K>,V extends Comparabl
 	@Override
 	public int hash(K key) 
 	{
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	
-	private int nextPrime(int num) 
-	{
-		num++;
-	      for (int i = 2; i < num; i++) {
-	         if(num%i == 0) {
-	            num++;
-	            i=2;
-	         } else {
-	            continue;
-	         }
-	      }
-	      return num;
-	}
-	
+	// Function that returns true if n
+    // is prime else returns false
+    static boolean isPrime(int n)
+    {
+        // Corner cases
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        // This is checked so that we can skip
+        // middle five numbers in below loop
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+        return true;
+    }
+
+    // Function to return the smallest
+    // prime number greater than N
+    static int nextPrime(int N)
+    {
+        // Base case
+        if (N <= 1)
+            return 2;
+        int prime = N;
+        boolean found = false;
+        // Loop continuously until isPrime returns
+        // true for a number greater than n
+        while (!found)
+        {
+            prime++;
+            if (isPrime(prime))
+                found = true;
+        }
+        return prime;
+
+    }
 	private int getNextEmpty(int posicion)
 	{
 		int p1 = posicion;

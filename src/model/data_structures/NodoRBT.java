f23c368d3a5e3dd4c0424b2ff9aa5bebb271c92a
JuanCamilo <juancamilo@MacBook-Pro-de-JuanCamilo.local>
2021-04-19 18:29:00 -0500

    Arreglos 2

--- src/model/data_structures/NodoRBT.java
+++ src/model/data_structures/NodoRBT.java
@@ -1,5 +1,7 @@
 package model.data_structures;
 
+import java.awt.Color;
+
 public class NodoRBT <K extends Comparable<K>, V extends Comparable<V>> implements Comparable<NodoRBT<K,V>>
 {
 	K llave;
@@ -7,14 +9,11 @@ public class NodoRBT <K extends Comparable<K>, V extends Comparable<V>> implemen
 	NodoRBT derecho, izquierdo;
 	int numeroNodos;
 
-	private static final String RED = "Rojo";
-	private static final String BLACK = "Negro";
-
-	String color;
+	public static Color color;
 
 	public NodoRBT(K llave, V valor)
 	{
-		color = RED;
+
 	}
 
 	public int compareTo(NodoRBT<K, V> otro)
@@ -31,15 +30,76 @@ public class NodoRBT <K extends Comparable<K>, V extends Comparable<V>> implemen
 		}
 		else 
 		{
-
+			if(comparacion < 0)
+			{
+				if(izquierdo != null)
+				{
+					izquierdo.put(llave, val);
+				}
+				else
+				{
+					izquierdo = new NodoRBT(llave, val);
+				}
+			}
+			else
+			{
+				if(derecho != null)
+				{
+					derecho.put(llave, val);
+				}
+				else
+				{
+					derecho = new NodoRBT(llave, val);
+				}
+			}
 		}
+		
+		NodoRBT<K, V> raiz = evaluarYcorregir();
+		raiz = raiz.evaluarYcorregir();
+		return raiz;
+	}
+
+	private NodoRBT<K, V> evaluarYcorregir()
+	{
+		NodoRBT<K, V> raizSubArbol = this;
+		NodoRBT<K, V> der = this.derecho;
+		NodoRBT<K, V> izq = this.izquierdo;
+		if(der != null && der.color == Color.RED)
+		{
+			if(izq != null && izq.color == Color.RED)
+			{
+				der.color = Color.BLACK;
+				izq.color = Color.BLACK;
+				this.color = Color.RED;
+			}
+			else
+			{
+				raizSubArbol = rotarIzquierda();
+			}
+		}
+		
+		if(izq != null && izq.color == Color.RED && izq.izquierdo != null )//Falta
+		{
+			raizSubArbol = rotarDerecha();
+		}
+		return raizSubArbol;
+	}
+
+	private NodoRBT<K, V> rotarDerecha() 
+	{
+
+		return null;
+	}
+
+	private NodoRBT<K, V> rotarIzquierda() 
+	{
 		return null;
 	}
 
 	public void cambiarColor(NodoRBT x) 
 	{
-		x.color = RED;
-		x.izquierdo.color = BLACK;
-		x.derecho.color = BLACK;
+		x.color = Color.RED;
+		x.izquierdo.color = Color.BLACK ;
+		x.derecho.color = Color.BLACK;
 	}	
 }
